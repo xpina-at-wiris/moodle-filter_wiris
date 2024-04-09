8428bf6bd2d7e0730db9d322838770922e4c2ba0
Xavier Piña <xavier.pina@wiris.com>
2024-04-09 13:16:34 +0200

    feat: new commands (new full-screen and new wait until)

--- tests/behat/behat_wiris_editor.php
+++ tests/behat/behat_wiris_editor.php
@@ -140,4 +140,60 @@ class behat_wiris_editor extends behat_wiris_base {
         $component->click();
     }
 
+    /**
+     * Click on MathType editor full screen button
+     *
+     * @Given I click on MathType editor full screen button
+     * @throws ExpectationException If the full screen button is not found, it will throw an exception.
+     */
+    public function i_click_on_mathtype_full_screen_button() {
+        $session = $this->getSession();
+        $component = $session->getPage()->find('xpath', '//a[@class=\'wrs_modal_maximize_button wrs_modal_desktop wrs_stack\']');
+        if (empty($component)) {
+            throw new ExpectationException('Full-screen button not found.', $this->getSession());
+        }
+        $component->click();
+    }
+
+    /**
+     * Follows the page redirection. Use this step after clicking the editor's maximize button
+     *
+     * @Then full screen modal window is opened
+     * @param  string $seconds time to wait
+     */
+    public function full_screen_modal_window_is_opened() {
+        $session = $this->getSession();
+        $component = $session->getPage()->find('xpath', '//div[contains(@class, "wrs_modal_overlay wrs_modal_desktop wrs_maximized")]');
+        if (empty($component) || !$component->isVisible()) {
+            throw new ExpectationException("Full-screen modal window is opened.", $this->getSession());
+        }
+    }
+
+    /**
+     * Look whether MathType editor exist
+     *
+     * @Then MathType editor should exist
+     * @throws ExpectationException If MathType editor not found, it will throw an exception.
+     */
+    public function mathtype_editor_should_exist() {
+        $session = $this->getSession();
+        $formula = $session->getPage()->find('xpath', '//div[contains(@id, \'wrs_modal_wrapper\')]');
+        if (empty($formula)) {
+            throw new ExpectationException('MathType editor not found.', $this->getSession());
+        }
+    }
+
+    /**
+     * Follows the page redirection. Use this step after clicking the editor's maximize button
+     *
+     * @Then i wait until MathType editor is displayed
+     */
+    public function i_wait_until_mathtype_editor_is_displayed() {
+        // Looks for a math formula in the page.
+        $formula = '//div[contains(@id, \'wrs_modal_wrapper\')]';
+        $this->ensure_element_exists($formula, 'xpath_element');
+        // Then re-validate to throw error otherwise (?).
+        $this->mathtype_editor_should_exist();
+    }
+
 }
